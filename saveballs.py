66666dfca1a9c5d74347a42bc4f0ae7d60768bc7
Shivam Maurya <shivam.m2826@gmail.com>
2017-08-06 20:51:49 +0530

    adding files

--- saveballs.py
+++ saveballs.py
@@ -1,5 +1,5 @@
 from time import sleep
-
+import shelve
 import pygame as pg
 from random import randint,randrange
 
@@ -19,42 +19,55 @@ qicon=pg.image.load('cross.png')
 ricon=pg.image.load('repeat.png')
 ts=pg.mixer.Sound('Tick.ogg')
 gbs=pg.mixer.Sound('glass_break.wav')
-#gbs.set_volume(.9)
-my_font = pg.font.SysFont("comicsansms", 24)
+my_font = pg.font.SysFont("comicsansms", 25,1)
 overfont = pg.font.SysFont("comicsansms", 56)
 startfont=pg.font.SysFont("comicsansms",85,1)
 byfont=pg.font.SysFont("comicsansms",15,1)
+hsfont=pg.font.SysFont("comicsansms",30,1)
 
+
+
+d = shelve.open('score.txt') # here you will save the score variable
+score=d['score']
 
 #magic=pg.image.load('magic.png')
 
-#FUNCTION DEFINITION
-def start_game():
-    while True:
-        window.fill([255, 240, 230])
-        window.blit(sicon,(200,336))
-        window.blit(qicon,(472,336))
-        start_text=startfont.render('Save the balls',True,[0,10,190])
-        window.blit(start_text,(window.get_width() / 2 - start_text.get_rect().width / 2, 170))
-        byme=byfont.render('By: Shivam Maurya',True,[0,0,0])
-        window.blit(byme,(630,560))
-        button_action(200,335,328,460,"play")
-        button_action(470,335,598,460,"quit")
-        for event in pg.event.get():
-            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
-                pg.QUIT
-                raise SystemExit
-        pg.display.update()
-
+#FUNCTION DEFINITIONS
 def button_action(p1,q1,p2,q2,do=None):
     drag=pg.mouse.get_pos()
     click=pg.mouse.get_pressed()
     if p1<drag[0]<p2 and q1<drag[1]<q2 and click[0]==1:
         if do=="play" or do=="reload":
             gameplay()
+        #elif do=="home":
+         #   goHome()
         elif do=="quit":
             pg.QUIT
             raise SystemExit
+
+
+def startgame(scr):
+    while True:
+        window.fill([255, 240, 230])
+        window.blit(sicon,(200,306))
+        window.blit(qicon,(472,306))
+        button_action(199, 305, 328, 434,"play")
+        button_action(471, 305, 600, 434,"quit")
+
+        start_text=startfont.render('Save the balls',True,[0,10,250])
+        window.blit(start_text,(window.get_width() / 2 - start_text.get_rect().width / 2, 140))
+        byme=byfont.render('By: Shivam Maurya',True,[0,0,0])
+        window.blit(byme,(630,560))
+        hscore = hsfont.render('Highest Score: '+str(scr), True, [0, 0, 0])
+        hstext=hscore.get_rect()
+        text_hs=window.get_width() / 2 - hstext.width / 2
+        window.blit(hscore, (text_hs, 500))
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.QUIT
+                raise SystemExit
+        pg.display.update()
 
 
 def moveball(x,y,xp,yp,bx,scr,gover):
@@ -71,29 +84,42 @@ def moveball(x,y,xp,yp,bx,scr,gover):
             ts.play()
             scr+=5
             yc = randrange(-6, -4, 1)
+            if xc==0:
+                if bx+110<x:
+                    xc=randint(1,3)
+                elif bx-112>x:
+                    xc=randrange(-2,-1,1)
+
     elif x<=5:
         xc=randint(3,4)
     elif y<=5:
         yc=randint(4,6)
     return gover,scr,xc,yc
 
+#def goHome():
+#    startgame()
+
 def gameover(scr):
+    global score
     window.fill([250, 240, 230])
     text = overfont.render('Game Over', True, [250, 0, 0])
     yscr = overfont.render('You scored: ' + str(scr), True, [0, 50, 250])
     text_rect = text.get_rect()
     text_x = window.get_width() / 2 - text_rect.width / 2
     window.blit(text, (text_x,150 ))
-    window.blit(yscr, (window.get_width() / 2 - yscr.get_rect().width / 2,236 ))
+    window.blit(yscr, (window.get_width()/2 - yscr.get_rect().width/2, 236 ))
     window.blit(ricon,(200,322))
     window.blit(qicon, (472, 322))
     button_action(200,332,328,460,"reload")
     button_action(470,332,598,460,"quit")
-    pg.display.update()
+    if score<scr:
+        d['score'] = scr
+
     for event in pg.event.get():
         if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
             pg.QUIT
             raise SystemExit
+    pg.display.update()
 
 def gameplay():
     scr = 0
@@ -158,6 +184,7 @@ def gameplay():
             pg.display.update()
             c.tick(callfps(scr))
 
+
 def callfps(scr):
     fps=60
     if scr<=30:
@@ -177,5 +204,7 @@ def callfps(scr):
     return fps
 
 
-start_game()
+startgame(score)
+           # thats all, now it is saved on disk.
+d.close()
 raise SystemExit
